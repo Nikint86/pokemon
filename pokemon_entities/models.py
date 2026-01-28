b8fd6c6a8d7e9f3f7ca6bf5dd2b3db3b0e69359f
Nikint86 <lobackiy_2007@mail.ru>
2026-01-28 14:56:27 +0300

    create Pokemon model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,8 @@
 from django.db import models  # noqa F401
 
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Покемон')
+
+    def __str__(self):
+        return self.title
 # your models here
